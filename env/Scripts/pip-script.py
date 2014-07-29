fe46e3d350482f8b526d8befde692af51e884aba
Michael Browne <arcticfeather@hotmail.com>
2014-07-29 15:23:09 +0900

    SQL chapter

---
+++ env/Scripts/pip-script.py
@@ -0,0 +1,9 @@
+#!"C:\Users\Mike\SkyDrive\Documents\Other\Python\Real Python\Book 2 files\sql\env\Scripts\python.EXE"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==1.3.1','console_scripts','pip'
+__requires__ = 'pip==1.3.1'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('pip==1.3.1', 'console_scripts', 'pip')()
+)
